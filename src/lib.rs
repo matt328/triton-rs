49149c3713ea2f0fa0cee4531dcddab0ef9e2f22
Matt Teeter <matt328@gmail.com>
2024-01-09 18:25:31 -0500

    Starting base code for if when things go south

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod app;
-pub mod game;
-pub mod graphics;
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,246 @@
-use anyhow::Context;
+#![allow(clippy::eq_op)]
 
-#[cfg(feature = "tracing")]
-use log::info;
-#[cfg(feature = "tracing")]
-use tracing::{span, Level};
-#[cfg(feature = "tracing")]
-use tracing_subscriber::layer::SubscriberExt;
-use triton::app::App;
+use std::sync::Arc;
 
-fn main() -> anyhow::Result<()> {
-    log4rs::init_file("log4rs.yml", Default::default()).context("Could not configure logger")?;
+use egui::{load::SizedTexture, Context, ImageSource, Visuals};
+use egui_winit_vulkano::{egui, Gui, GuiConfig};
+use vulkano::{
+    command_buffer::allocator::{
+        StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo,
+    },
+    format::Format,
+    image::{view::ImageView, Image, ImageCreateInfo, ImageType, ImageUsage},
+    memory::allocator::AllocationCreateInfo,
+    sync::{self, GpuFuture},
+};
+use vulkano_util::{
+    context::{VulkanoConfig, VulkanoContext},
+    renderer::DEFAULT_IMAGE_FORMAT,
+    window::{VulkanoWindows, WindowDescriptor},
+};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
 
-    #[cfg(feature = "tracing")]
-    info!("Tracing enabled");
+use crate::{renderer::RenderPipeline, time_info::TimeInfo};
 
-    #[cfg(feature = "tracing")]
-    #[global_allocator]
-    static GLOBAL: tracy_client::ProfiledAllocator<std::alloc::System> =
-        tracy_client::ProfiledAllocator::new(std::alloc::System, 100);
+mod frame_system;
+mod renderer;
+mod time_info;
+mod triangle_draw_system;
 
-    #[cfg(feature = "tracing")]
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::registry().with(tracing_tracy::TracyLayer::new()),
+/// Example struct to contain the state of the UI
+pub struct GuiState {
+    show_texture_window1: bool,
+    show_texture_window2: bool,
+    show_scene_window: bool,
+    image_texture_id1: egui::TextureId,
+    image_texture_id2: egui::TextureId,
+    scene_texture_id: egui::TextureId,
+    scene_view_size: [u32; 2],
+}
+
+impl GuiState {
+    pub fn new(gui: &mut Gui, scene_image: Arc<ImageView>, scene_view_size: [u32; 2]) -> GuiState {
+        // tree.png asset is from https://github.com/sotrh/learn-wgpu/tree/master/docs/beginner/tutorial5-textures
+        let image_texture_id1 = gui.register_user_image(
+            include_bytes!("./assets/tree.png"),
+            Format::R8G8B8A8_SRGB,
+            Default::default(),
+        );
+        let image_texture_id2 = gui.register_user_image(
+            include_bytes!("./assets/doge2.png"),
+            Format::R8G8B8A8_SRGB,
+            Default::default(),
+        );
+
+        GuiState {
+            show_texture_window1: true,
+            show_texture_window2: true,
+            show_scene_window: true,
+            image_texture_id1,
+            image_texture_id2,
+            scene_texture_id: gui.register_user_image_view(scene_image, Default::default()),
+            scene_view_size,
+        }
+    }
+
+    /// Defines the layout of our UI
+    pub fn layout(&mut self, egui_context: Context, window_size: [f32; 2], fps: f32) {
+        let GuiState {
+            show_texture_window1,
+            show_texture_window2,
+            show_scene_window,
+            image_texture_id1,
+            image_texture_id2,
+            scene_view_size,
+            scene_texture_id,
+            ..
+        } = self;
+        egui_context.set_visuals(Visuals::dark());
+        egui::SidePanel::left("Side Panel")
+            .default_width(150.0)
+            .show(&egui_context, |ui| {
+                ui.heading("Hello Tree");
+                ui.separator();
+                ui.checkbox(show_texture_window1, "Show Tree");
+                ui.checkbox(show_texture_window2, "Show Doge");
+                ui.checkbox(show_scene_window, "Show Scene");
+            });
+
+        egui::Window::new("Mah Tree")
+            .resizable(true)
+            .vscroll(true)
+            .open(show_texture_window1)
+            .show(&egui_context, |ui| {
+                ui.image(ImageSource::Texture(SizedTexture::new(
+                    *image_texture_id1,
+                    [256.0, 256.0],
+                )));
+            });
+        egui::Window::new("Mah Doge")
+            .resizable(true)
+            .vscroll(true)
+            .open(show_texture_window2)
+            .show(&egui_context, |ui| {
+                ui.image(ImageSource::Texture(SizedTexture::new(
+                    *image_texture_id2,
+                    [300.0, 200.0],
+                )));
+            });
+        egui::Window::new("Scene")
+            .resizable(true)
+            .vscroll(true)
+            .open(show_scene_window)
+            .show(&egui_context, |ui| {
+                ui.image(ImageSource::Texture(SizedTexture::new(
+                    *scene_texture_id,
+                    [scene_view_size[0] as f32, scene_view_size[1] as f32],
+                )));
+            });
+        egui::Area::new("fps")
+            .fixed_pos(egui::pos2(window_size[0] - 0.05 * window_size[0], 10.0))
+            .show(&egui_context, |ui| {
+                ui.label(format!("{fps:.2}"));
+            });
+    }
+}
+
+pub fn main() -> anyhow::Result<()> {
+    // Winit event loop & our time tracking initialization
+    let event_loop = EventLoop::new();
+    let mut time = TimeInfo::new();
+    // Create renderer for our scene & ui
+    let scene_view_size = [256, 256];
+    // Vulkano context
+    let context = VulkanoContext::new(VulkanoConfig::default());
+    // Vulkano windows (create one)
+    let mut windows = VulkanoWindows::default();
+    windows.create_window(&event_loop, &context, &WindowDescriptor::default(), |ci| {
+        ci.image_format = Format::B8G8R8A8_UNORM;
+        ci.min_image_count = ci.min_image_count.max(2);
+    });
+    // Create gui as main render pass (no overlay means it clears the image each frame)
+    let mut gui = {
+        let renderer = windows.get_primary_renderer_mut().unwrap();
+        Gui::new(
+            &event_loop,
+            renderer.surface(),
+            renderer.graphics_queue(),
+            renderer.swapchain_format(),
+            GuiConfig::default(),
+        )
+    };
+    // Create a simple image to which we'll draw the triangle scene
+    let scene_image = ImageView::new_default(
+        Image::new(
+            context.memory_allocator().clone(),
+            ImageCreateInfo {
+                image_type: ImageType::Dim2d,
+                format: DEFAULT_IMAGE_FORMAT,
+                extent: [scene_view_size[0], scene_view_size[1], 1],
+                array_layers: 1,
+                usage: ImageUsage::SAMPLED | ImageUsage::COLOR_ATTACHMENT,
+                ..Default::default()
+            },
+            AllocationCreateInfo::default(),
+        )
+        .unwrap(),
     )
-    .expect("setting up tracing");
+    .unwrap();
 
-    #[cfg(feature = "tracing")]
-    let _root = span!(Level::INFO, "root").entered();
+    // Create our render pipeline
+    let mut scene_render_pipeline = RenderPipeline::new(
+        context.graphics_queue().clone(),
+        DEFAULT_IMAGE_FORMAT,
+        &renderer::Allocators {
+            command_buffers: Arc::new(StandardCommandBufferAllocator::new(
+                context.device().clone(),
+                StandardCommandBufferAllocatorCreateInfo {
+                    secondary_buffer_count: 32,
+                    ..Default::default()
+                },
+            )),
+            memory: context.memory_allocator().clone(),
+        },
+    );
+    // Create gui state (pass anything your state requires)
+    let mut gui_state = GuiState::new(&mut gui, scene_image.clone(), scene_view_size);
+    // Event loop run
+    event_loop.run(move |event, _, control_flow| {
+        let renderer = windows.get_primary_renderer_mut().unwrap();
+        // Update Egui integration so the UI works!
+        match event {
+            Event::WindowEvent { event, window_id } if window_id == renderer.window().id() => {
+                let _pass_events_to_game = !gui.update(&event);
+                match event {
+                    WindowEvent::Resized(_) => {
+                        renderer.resize();
+                    }
+                    WindowEvent::ScaleFactorChanged { .. } => {
+                        renderer.resize();
+                    }
+                    WindowEvent::CloseRequested => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                    _ => (),
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == window_id => {
+                // Set immediate UI in redraw here
+                // It's a closure giving access to egui context inside which you can call anything.
+                // Here we're calling the layout of our `gui_state`.
+                gui.immediate_ui(|gui| {
+                    let ctx = gui.context();
+                    gui_state.layout(ctx, renderer.window_size(), time.fps())
+                });
+                // Render UI
+                // Acquire swapchain future
+                let before_future = match renderer.acquire() {
+                    Ok(future) => future,
+                    Err(vulkano::VulkanError::OutOfDate) => {
+                        renderer.resize();
+                        sync::now(context.device().clone()).boxed()
+                    }
+                    Err(e) => panic!("Failed to acquire swapchain future: {}", e),
+                };
+                // Draw scene
+                let after_scene_draw =
+                    scene_render_pipeline.render(before_future, scene_image.clone());
+                // Render gui
+                let after_future =
+                    gui.draw_on_image(after_scene_draw, renderer.swapchain_image_view());
+                // Present swapchain
+                renderer.present(after_future, true);
 
-    let app = App::new()?;
-
-    app.run()
+                // Update fps & dt
+                time.update();
+            }
+            Event::MainEventsCleared => {
+                renderer.window().request_redraw();
+            }
+            _ => (),
+        }
+    })
 }
